317cea656728d9bdc185fc7546a599926faa4ed7
Kyuuhachi <caagr98@gmail.com>
2023-11-15 22:58:13 +0100

    Add example
    More for testing than as an actual example but still

---
+++ examples/scena.rs
@@ -0,0 +1,15 @@
+use themelios::scena::ed6;
+use themelios::scena::insn_set::Builtin;
+
+fn main() -> anyhow::Result<()> {
+	unsafe { compact_debug::enable(true) }
+
+	let iset = Builtin::Fc.get();
+
+	for file in std::env::args().skip(1) {
+		let scena = ed6::Scena::read(iset, &std::fs::read(file)?)?;
+		println!("{:#?}", scena);
+	}
+
+	Ok(())
+}
