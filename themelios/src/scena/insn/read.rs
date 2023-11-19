80158def8a5af15abec31b2b8379272ccc6d8f07
Kyuuhachi <caagr98@gmail.com>
2023-11-19 02:51:39 +0100

    Refactor insn::read to use an InsnReader

--- themelios/src/scena/insn/read.rs
+++ themelios/src/scena/insn/read.rs
@@ -30,209 +30,188 @@ pub enum ReadError {
 
 type Result<T, E = ReadError> = std::result::Result<T, E>;
 
+struct InsnReader<'iset, 'read, 'buf> {
+	f: &'read mut Reader<'buf>,
+	iset: &'iset iset::InsnSet,
+}
+
 pub(crate) fn read(f: &mut Reader, iset: &iset::InsnSet) -> Result<Insn> {
-	let mut args = Vec::new();
-	let byte = f.u8()? as usize;
-	let name = read_insn(f, &mut args, iset, &iset.insns, byte)?;
-	Ok(Insn::new(name, args))
+	let mut reader = InsnReader { f, iset };
+	reader.insn()
 }
 
-fn read_insn<'a>(
-	f: &mut Reader,
-	out: &mut Vec<Arg>,
-	iset: &'a iset::InsnSet,
-	insns: &'a [iset::Insn],
-	n: usize,
-) -> Result<&'a str> {
-	match insns.get(n) {
-		None | Some(iset::Insn::Blank) => whatever!("unknown instruction {n:02X}"),
-		Some(iset::Insn::Regular { name, args }) => {
-			for arg in args {
-				read_arg(f, out, iset, arg)?;
+impl<'iset, 'read, 'buf> InsnReader<'iset, 'read, 'buf> {
+	fn insn(&mut self) -> Result<Insn> {
+		let mut args = Vec::new();
+		let byte = self.f.u8()? as usize;
+		let name = self.insn_inner(&mut args, &self.iset.insns, byte)?;
+		Ok(Insn::new(name, args))
+	}
+
+	fn insn_inner(
+		&mut self,
+		out: &mut Vec<Arg>,
+		insns: &'iset [iset::Insn],
+		n: usize,
+	) -> Result<&'iset str> {
+		match insns.get(n) {
+			None | Some(iset::Insn::Blank) => whatever!("unknown instruction {n:02X}"),
+			Some(iset::Insn::Regular { name, args }) => {
+				for arg in args {
+					self.arg(out, arg)?;
+				}
+				Ok(name)
 			}
-			Ok(name)
-		}
-		Some(iset::Insn::Match { head, on, cases }) => {
-			for arg in head {
-				read_arg(f, out, iset, arg)?;
+			Some(iset::Insn::Match { head, on, cases }) => {
+				for arg in head {
+					self.arg(out, arg)?;
+				}
+				let n = self.int(*on)? as usize;
+				self.insn_inner(out, cases, n)
 			}
-			let n = read_int(f, *on)? as usize;
-			read_insn(f, out, iset, cases, n)
 		}
 	}
-}
 
-fn read_int(f: &mut Reader, int: iset::IntType) -> Result<i64> {
-	Ok(match int {
-		iset::IntType::u8 => f.u8()? as i64,
-		iset::IntType::u16 => f.u16()? as i64,
-		iset::IntType::u24 => (f.u16()? as i64) | (f.u8()? as i64) << 8,
-		iset::IntType::u32 => f.u32()? as i64,
-		iset::IntType::i8 => f.i8()? as i64,
-		iset::IntType::i16 => f.i16()? as i64,
-		iset::IntType::i32 => f.i32()? as i64,
-		iset::IntType::Const(v) => v,
-	})
-}
-
-fn read_arg(
-	f: &mut Reader,
-	out: &mut Vec<Arg>,
-	iset: &iset::InsnSet,
-	arg: &iset::Arg,
-) -> Result<(), ReadError> {
-	match arg {
-		iset::Arg::Int(int, ty) => {
-			let v = read_int(f, *int)?;
-			out.extend(parse_int_arg(f, iset, v, *ty)?)
-		}
-		iset::Arg::Misc(ty) => read_misc(f, out, iset, *ty)?,
-		iset::Arg::Tuple(args) => {
-			let mut values = Vec::new();
-			for arg in args {
-				read_arg(f, &mut values, iset, arg)?;
-			}
-			out.push(Arg::Tuple(values))
-		}
+	fn int(&mut self, int: iset::IntType) -> Result<i64> {
+		let f = &mut self.f;
+		Ok(match int {
+			iset::IntType::u8 => f.u8()? as i64,
+			iset::IntType::u16 => f.u16()? as i64,
+			iset::IntType::u24 => (f.u16()? as i64) | (f.u8()? as i64) << 8,
+			iset::IntType::u32 => f.u32()? as i64,
+			iset::IntType::i8 => f.i8()? as i64,
+			iset::IntType::i16 => f.i16()? as i64,
+			iset::IntType::i32 => f.i32()? as i64,
+			iset::IntType::Const(v) => v,
+		})
 	}
-	Ok(())
-}
 
-fn read_misc(
-	f: &mut Reader,
-	out: &mut Vec<Arg>,
-	iset: &iset::InsnSet,
-	ty: iset::MiscArg,
-) -> Result<()> {
-	use iset::MiscArg as T;
-	match ty {
-		T::String => out.push(Arg::String(f.string()?)),
-		T::TString => out.push(Arg::TString(TString(f.string()?))),
-		T::Text => parse_text(f, out)?,
-
-		T::Pos2 => out.push(Arg::Pos2(Pos2 {
-			x: f.i32()?,
-			z: f.i32()?,
-		})),
-		T::Pos3 => out.push(Arg::Pos3(Pos3 {
-			x: f.i32()?,
-			y: f.i32()?,
-			z: f.i32()?,
-		})),
-		T::RPos3 => out.push(Arg::RPos3(Pos3 {
-			x: f.i32()?,
-			y: f.i32()?,
-			z: f.i32()?,
-		})),
-
-		T::Expr => out.push(Arg::Expr(Expr::read(f, iset).map_err(Box::new)?)),
-
-		T::Fork => {
-			let len = f.u8()? as usize;
-			let pos = f.pos();
-			let code = Code::read(f, iset, Some(pos + len)).map_err(Box::new)?;
-			if len > 0 {
-				f.check_u8(0)?;
+	fn arg(
+		&mut self,
+		out: &mut Vec<Arg>,
+		arg: &iset::Arg,
+	) -> Result<(), ReadError> {
+		match arg {
+			iset::Arg::Int(int, ty) => {
+				let v = self.int(*int)?;
+				out.extend(int_arg(self.iset, v, *ty)?)
 			}
-			out.push(Arg::Code(code))
-		}
-
-		T::ForkLoop => {
-			let len = f.u8()? as usize;
-			let pos = f.pos();
-			let code = Code::read(f, iset, Some(pos + len)).map_err(Box::new)?;
-
-			let insns = [read(f, iset)?, read(f, iset)?];
-			#[rustfmt::skip]
-			ensure_whatever!(
-				insns == [
-					Insn::new(&iset.fork_loop_next, vec![]),
-					Insn::new("_goto", vec![Arg::Label(pos)]),
-				],
-				"invalid ForkLoop: ended with {insns:?}"
-			);
-			out.push(Arg::Code(code))
-		}
-
-		T::SwitchTable => {
-			let count = read_int(f, iset.switch_table_size)? as usize;
-			let mut cs = Vec::with_capacity(count);
-			for _ in 0..count {
-				read_arg(f, &mut cs, iset, &iset.switch_table_type)?;
-			}
-			out.push(Arg::Tuple(cs))
-		}
-
-		T::QuestList => loop {
-			match f.u16()? {
-				0xFFFF => break,
-				q => out.push(Arg::Quest(QuestId(q))),
-			}
-		},
-
-		T::Menu => {
-			for line in f.string()?.split_terminator('\x01') {
-				out.push(Arg::TString(TString(line.to_owned())))
+			iset::Arg::Misc(ty) => self.misc(out, *ty)?,
+			iset::Arg::Tuple(args) => {
+				let mut values = Vec::new();
+				for arg in args {
+					self.arg(&mut values, arg)?;
+				}
+				out.push(Arg::Tuple(values))
 			}
 		}
+		Ok(())
+	}
 
-		T::FcPartyEquip => {
-			if matches!(out[1], Arg::Item(ItemId(600..=799))) {
-				out.push(Arg::Int(f.u8()? as _))
-			} else {
-				out.push(Arg::Int(0))
-			}
-		}
+	fn misc(
+		&mut self,
+		out: &mut Vec<Arg>,
+		ty: iset::MiscArg,
+	) -> Result<()> {
+		let f = &mut self.f;
+		use iset::MiscArg as T;
+		match ty {
+			T::String => out.push(Arg::String(f.string()?)),
+			T::TString => out.push(Arg::TString(TString(f.string()?))),
+			T::Text => text(f, out)?,
 
-		T::EffPlayPos => {
-			let pos = Pos3 {
+			T::Pos2 => out.push(Arg::Pos2(Pos2 {
+				x: f.i32()?,
+				z: f.i32()?,
+			})),
+			T::Pos3 => out.push(Arg::Pos3(Pos3 {
 				x: f.i32()?,
 				y: f.i32()?,
 				z: f.i32()?,
-			};
-			if matches!(out[0], Arg::Char(CharId::Null)) {
-				out.push(Arg::Pos3(pos))
-			} else {
-				out.push(Arg::RPos3(pos))
+			})),
+			T::RPos3 => out.push(Arg::RPos3(Pos3 {
+				x: f.i32()?,
+				y: f.i32()?,
+				z: f.i32()?,
+			})),
+
+			T::Expr => out.push(Arg::Expr(Expr::read(f, self.iset).map_err(Box::new)?)),
+
+			T::Fork => {
+				let len = f.u8()? as usize;
+				let pos = f.pos();
+				let code = Code::read(f, self.iset, Some(pos + len)).map_err(Box::new)?;
+				if len > 0 {
+					f.check_u8(0)?;
+				}
+				out.push(Arg::Code(code))
+			}
+
+			T::ForkLoop => {
+				let len = f.u8()? as usize;
+				let pos = f.pos();
+				let code = Code::read(f, self.iset, Some(pos + len)).map_err(Box::new)?;
+
+				let insns = [self.insn()?, self.insn()?];
+				#[rustfmt::skip]
+				ensure_whatever!(
+					insns == [
+						Insn::new(&self.iset.fork_loop_next, vec![]),
+						Insn::new("_goto", vec![Arg::Label(pos)]),
+					],
+					"invalid ForkLoop: ended with {insns:?}"
+				);
+				out.push(Arg::Code(code))
+			}
+
+			T::SwitchTable => {
+				let count = self.int(self.iset.switch_table_size)? as usize;
+				let mut cs = Vec::with_capacity(count);
+				for _ in 0..count {
+					self.arg(&mut cs, &self.iset.switch_table_type)?;
+				}
+				out.push(Arg::Tuple(cs))
+			}
+
+			T::QuestList => loop {
+				match f.u16()? {
+					0xFFFF => break,
+					q => out.push(Arg::Quest(QuestId(q))),
+				}
+			},
+
+			T::Menu => {
+				for line in f.string()?.split_terminator('\x01') {
+					out.push(Arg::TString(TString(line.to_owned())))
+				}
+			}
+
+			T::FcPartyEquip => {
+				if matches!(out[1], Arg::Item(ItemId(600..=799))) {
+					out.push(Arg::Int(f.u8()? as _))
+				} else {
+					out.push(Arg::Int(0))
+				}
+			}
+
+			T::EffPlayPos => {
+				let pos = Pos3 {
+					x: f.i32()?,
+					y: f.i32()?,
+					z: f.i32()?,
+				};
+				if matches!(out[0], Arg::Char(CharId::Null)) {
+					out.push(Arg::Pos3(pos))
+				} else {
+					out.push(Arg::RPos3(pos))
+				}
 			}
 		}
+		Ok(())
 	}
-	Ok(())
 }
 
-fn parse_text(f: &mut Reader, out: &mut Vec<Arg>) -> Result<()> {
-	loop {
-		let (page, more) = parse_text_page(f)?;
-		out.push(Arg::Text(page));
-		if !more {
-			break;
-		}
-	}
-	Ok(())
-}
-
-fn parse_text_page(f: &mut Reader) -> Result<(TString, bool)> {
-	let mut buf = Vec::new();
-	let more = loop {
-		match f.u8()? {
-			0x00 => break false,
-			0x01 => buf.extend(b"\\n"), // newline → line feed
-			0x02 => buf.extend(b"\\f"), // pause → page break
-			0x03 => break true,         // page break
-			0x07 => buf.extend(format!("\\{}C", f.u8()?).as_bytes()),
-			0x0D => buf.extend(b"\\r"),
-			0x1F => buf.extend(format!("\\{}i", f.u16()?).as_bytes()),
-			ch @ (0x00..=0x1F) => buf.extend(format!("\\{}x", ch).as_bytes()),
-			b'\\' => buf.extend(b"\\\\"),
-			ch @ 0x20.. => buf.push(ch),
-		}
-	};
-	let string = crate::util::decode(&buf)?;
-	Ok((TString(string), more))
-}
-
-fn parse_int_arg(
-	f: &mut Reader,
+fn int_arg(
 	iset: &iset::InsnSet,
 	v: i64,
 	ty: iset::IntArg,
@@ -305,4 +284,35 @@ fn parse_int_arg(
 		T::CharFlags => Arg::CharFlags(cast(v)?),
 		T::CharFlags2 => Arg::CharFlags2(cast(v)?),
 	}))
+}
+
+fn text(f: &mut Reader, out: &mut Vec<Arg>) -> Result<()> {
+	loop {
+		let (page, more) = text_page(f)?;
+		out.push(Arg::Text(page));
+		if !more {
+			break;
+		}
+	}
+	Ok(())
+}
+
+fn text_page(f: &mut Reader) -> Result<(TString, bool)> {
+	let mut buf = Vec::new();
+	let more = loop {
+		match f.u8()? {
+			0x00 => break false,
+			0x01 => buf.extend(b"\\n"), // newline → line feed
+			0x02 => buf.extend(b"\\f"), // pause → page break
+			0x03 => break true,         // page break
+			0x07 => buf.extend(format!("\\{}C", f.u8()?).as_bytes()),
+			0x0D => buf.extend(b"\\r"),
+			0x1F => buf.extend(format!("\\{}i", f.u16()?).as_bytes()),
+			ch @ (0x00..=0x1F) => buf.extend(format!("\\{}x", ch).as_bytes()),
+			b'\\' => buf.extend(b"\\\\"),
+			ch @ 0x20.. => buf.push(ch),
+		}
+	};
+	let string = crate::util::decode(&buf)?;
+	Ok((TString(string), more))
 }
